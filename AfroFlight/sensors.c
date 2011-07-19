acdb43fcfef32662f1f83571fec1b8a6bd80b04d
dongie <trollcop@users.noreply.github.com>
2011-07-19 16:24:04 +0000

    switched adxl to fifo mode. read + average 8 times. switched to 3200hz rate, now getting 6-7 reads avg. changed adc logic to run 8 sample loops contiunously, then average. doubled SPI speed.

--- AfroFlight/sensors.c
+++ AfroFlight/sensors.c
@@ -7,6 +7,7 @@
 #define ADXL_MULTI_BIT     0x40
 #define ADXL_X0_ADDR       0x32
 #define ADXL_RATE_ADDR     0x2C
+#define ADXL_FIFO_ADDR     0x38
 #define ADXL_RATE_100      0x0A
 #define ADXL_RATE_200      0x0B
 #define ADXL_RATE_400      0x0C
@@ -22,6 +23,7 @@
 #define ADXL_RANGE_4G      0x01
 #define ADXL_RANGE_8G      0x02
 #define ADXL_RANGE_16G     0x03
+#define ADXL_FIFO_STREAM   0x80
 
 /* Gyro Pitch/Roll/Yaw final measurements */
 vs16 gyro[3] = { 512, 512, 512 };
@@ -37,9 +39,6 @@ s16 gyroZero[3] = { 0, 0, 0 };                                // used for calibr
 static vs16 sensorInputs[7] = { 0, };
 /* Are we currently reading ADC data? */
 static vu8 isAdcReading = FALSE;
-
-/* Gyro Pitch/Roll/Yaw, Battery Voltage, Acc X/Y/Z values */
-// vs16 ADCValues[7] = { 0, };
 
 static u16 batteryWarning;	// Battery Warning Voltage
 static u16 voltageLevel = 100;	// Battery Voltage
@@ -74,23 +73,34 @@ static u8 ADXL_ReadByte(void)
 static void ADXL_Init(void)
 {
     // setup ADXL345 rate/range/start measuring
+    
+    // Rate
     ADXL_ON;
     ADXL_WriteByte(ADXL_RATE_ADDR);
-    ADXL_WriteByte(ADXL_RATE_800 & 0x0F);
+    ADXL_WriteByte(ADXL_RATE_3200 & 0x0F);
     ADXL_OFF;
+
+    // Range
     ADXL_ON;
     ADXL_WriteByte(ADXL_FORMAT_ADDR);
     ADXL_WriteByte((ADXL_RANGE_4G & 0x03) | ADXL_FULL_RES | ADXL_4WIRE);
     ADXL_OFF;
+
+    // Fifo depth = 16
+    ADXL_ON;
+    ADXL_WriteByte(ADXL_FIFO_ADDR);
+    ADXL_WriteByte((16 & 0x1f) | ADXL_FIFO_STREAM);
+    ADXL_OFF;
+
     ADXL_ON;
     ADXL_WriteByte(ADXL_POWER_ADDR);
     ADXL_WriteByte(ADXL_MEASURE);
     ADXL_OFF;
 }
 
-static void ADXL_GetAccelValues(void)
+static u8 ADXL_GetAccelValues(void)
 {
-    u8 i;
+    vu8 i;
     
     ADXL_ON;
     ADXL_WriteByte(ADXL_X0_ADDR | ADXL_MULTI_BIT | ADXL_READ_BIT);
@@ -99,10 +109,14 @@ static void ADXL_GetAccelValues(void)
         i1 = ADXL_ReadByte();
         i2 = ADXL_ReadByte();
         sensorInputs[i + 4] += (i1 | i2 << 8);
-        // TODO remove
-        // ADCValues[i + 4] = i1 | i2 << 8;
     }
+    // skip over this register
+    ADXL_ReadByte();
+    // FIFO_STATUS register (last few bits = fifo remaining)
+    i = ADXL_ReadByte();
     ADXL_OFF;
+
+    return i & 0x7F; // return number of entires left in fifo
 }
 
 /*
@@ -116,16 +130,22 @@ static void ADXL_GetAccelValues(void)
 __near __interrupt void ADC1_IRQHandler(void)
 {
     u8 i = 0;
-
-    for (i = 0; i < 4; i++) {
+    static vu8 count = 0;
+    
+    // Get 4 ADC readings from buffer
+    for (i = 0; i < 4; i++)
         sensorInputs[i] += ADC1_GetBufferValue(i);
-        // TODO remove
-        // ADCValues[i] = ADC1_GetBufferValue(i);
-    }
 
     ADC1_ClearITPendingBit(ADC1_CSR_EOC);
-    isAdcReading = FALSE;
-    // ADC1_StartConversion();
+
+    // loop 8 times
+    count++;
+    if (count < 8)
+        ADC1_StartConversion();
+    else {
+        count = 0;
+        isAdcReading = FALSE;
+    }
 }
 
 /* Public functions */
@@ -133,7 +153,7 @@ void Sensors_Init(void)
 {
     // SPI
     SPI_DeInit();
-    SPI_Init(SPI_FIRSTBIT_MSB, SPI_BAUDRATEPRESCALER_4, SPI_MODE_MASTER, SPI_CLOCKPOLARITY_HIGH, SPI_CLOCKPHASE_2EDGE, SPI_DATADIRECTION_2LINES_FULLDUPLEX, SPI_NSS_SOFT, 0x07);
+    SPI_Init(SPI_FIRSTBIT_MSB, SPI_BAUDRATEPRESCALER_2, SPI_MODE_MASTER, SPI_CLOCKPOLARITY_HIGH, SPI_CLOCKPHASE_2EDGE, SPI_DATADIRECTION_2LINES_FULLDUPLEX, SPI_NSS_SOFT, 0x07);
     SPI_Cmd(ENABLE);
     
     // SPI ChipSelect for Accel
@@ -167,7 +187,7 @@ void Gyro_Calibrate(void)
     u8 i, j;
     u16 gyroCheck[8 * 3];       // gyro offset calc
     s16 gyroDiff = 0;
-
+    
     do {
         for (i = 0; i < 3; i++)
             gyroZero[i] = 0;
@@ -204,6 +224,7 @@ void Gyro_Calibrate(void)
                 gyroDiff += abs(temp);
             }
 	}
+        printf("GyroDiff: %d\r\n", (u16)gyroDiff);
         if (gyroDiff > 8)
             Beep(2, 50, 10);
     } while (gyroDiff > 8);
@@ -215,29 +236,32 @@ void Gyro_Calibrate(void)
 void Sensors_Read(void)
 {
     u8 i = 0;
-    
-    for (i = 0; i < 8; i++) {
-        // Gyro, Voltage
-        isAdcReading = TRUE;
-        ADC1_StartConversion();
-        // Wait
-        while (isAdcReading);    
-        // Accel
-        ADXL_GetAccelValues();
-    }
+    u8 remaining = 0, count = 0;
 
-    // gyro
+    // Fire off 8 adc reads (Gyro, Voltage)
+    isAdcReading = TRUE;
+    ADC1_StartConversion();
+    // Wait for the sample loop to complete
+    while (isAdcReading);
+
+    // average 8 gyro readings
     gyro[0] = (sensorInputs[0] >> 3); // - gyroZero[0];
     gyro[1] = (sensorInputs[1] >> 3); // - gyroZero[1];
     gyro[2] = (sensorInputs[2] >> 3); // - gyroZero[2];
+    
+    // Next up is accel fifo + avg
+    do {
+        count++;
+        remaining = ADXL_GetAccelValues();
+    } while ((count < 32) && (remaining > 0));
+
+    // accel + average
+    acc[0] = sensorInputs[4] / count;
+    acc[1] = sensorInputs[5] / count;
+    acc[2] = sensorInputs[6] / count;
 
     // 3.3V reference
     battery = (3 * battery + (11 * (sensorInputs[3] >> 3)) / 30) / 4;
-
-    // accel
-    acc[0] = sensorInputs[4] >> 3;
-    acc[1] = sensorInputs[5] >> 3;
-    acc[2] = sensorInputs[6] >> 3;
 
     // clear for next pass
     for (i = 0; i < 8; i++)
