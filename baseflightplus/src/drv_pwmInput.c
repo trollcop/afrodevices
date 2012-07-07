787921ee69afa9b26d9ea47da8ec19a86ed5ed14
jihlein <jihlein@users.noreply.github.com>
2012-07-07 13:48:08 +0000

    In BaseFlightPlus:
    1)Updated HEX6P and HEX6X mixers for even control power.  Used floating point equivalent of MultiWii's update
    2)Updated Serial and Parallel PWM input routines per rcsnow's optimization detailed at http://www.rcgroups.com/forums/showpost.php?p=22053386&postcount=161

--- baseflightplus/src/drv_pwmInput.c
+++ baseflightplus/src/drv_pwmInput.c
@@ -38,38 +38,38 @@ static struct TIM_Channel {
     uint16_t channel;
     uint16_t cc;
 } Channels[] = {
-    {
-    TIM2, TIM_Channel_1, TIM_IT_CC1}, {
-    TIM2, TIM_Channel_2, TIM_IT_CC2}, {
-    TIM2, TIM_Channel_3, TIM_IT_CC3}, {
-    TIM2, TIM_Channel_4, TIM_IT_CC4}, {
-    TIM3, TIM_Channel_1, TIM_IT_CC1}, {
-    TIM3, TIM_Channel_2, TIM_IT_CC2}, {
-    TIM3, TIM_Channel_3, TIM_IT_CC3}, {
-TIM3, TIM_Channel_4, TIM_IT_CC4},};
+    { TIM2, TIM_Channel_1, TIM_IT_CC1 },
+    { TIM2, TIM_Channel_2, TIM_IT_CC2 },
+    { TIM2, TIM_Channel_3, TIM_IT_CC3 },
+    { TIM2, TIM_Channel_4, TIM_IT_CC4 },
+    { TIM3, TIM_Channel_1, TIM_IT_CC1 },
+    { TIM3, TIM_Channel_2, TIM_IT_CC2 },
+    { TIM3, TIM_Channel_3, TIM_IT_CC3 },
+    { TIM3, TIM_Channel_4, TIM_IT_CC4 },
+};
 
 static struct PWM_State {
-    uint8_t state;
+    uint8_t  state;
     uint16_t rise;
     uint16_t fall;
     uint16_t capture;
-} Inputs[8] = { {
-0,}};
+} Inputs[8] = { { 0, } };
 
-static TIM_ICInitTypeDef TIM_ICInitStructure = { 0, };
+static TIM_ICInitTypeDef  TIM_ICInitStructure = { 0, };
 
 uint8_t rcActive = false;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-static void serialPWM_IRQHandler(TIM_TypeDef * tim)
+static void serialPWM_IRQHandler(TIM_TypeDef *tim)
 {
     uint16_t diff;
     static uint16_t now;
     static uint16_t last = 0;
-    static uint8_t chan = 0;
+    static uint8_t  chan = 0;
 
-    if (TIM_GetITStatus(tim, TIM_IT_CC1) == SET) {
+    if (TIM_GetITStatus(tim, TIM_IT_CC1) == SET)
+    {
         last = now;
         now = TIM_GetCapture1(tim);
         rcActive = true;
@@ -77,17 +77,15 @@ static void serialPWM_IRQHandler(TIM_TypeDef * tim)
 
     TIM_ClearITPendingBit(tim, TIM_IT_CC1);
 
-    if (now > last) {
-        diff = (now - last);
-    } else {
-        diff = ((0xFFFF - last) + now);
-    }
+    diff = now - last;
 
-    if (diff > 2700 * 2)        // Per http://www.rcgroups.com/forums/showpost.php?p=21996147&postcount=3960
-    {                           // "So, if you use 2.5ms or higher as being the reset for the PPM stream start,
-        chan = 0;               // you will be fine. I use 2.7ms just to be safe."
-    } else {
-        if (diff > 750 * 2 && diff < 2250 * 2 && chan < 8)      // 750 to 2250 ms is our 'valid' channel range
+    if (diff > 2700 * 2)   // Per http://www.rcgroups.com/forums/showpost.php?p=21996147&postcount=3960
+    {                      // "So, if you use 2.5ms or higher as being the reset for the PPM stream start,
+        chan = 0;          // you will be fine. I use 2.7ms just to be safe."
+    }
+    else
+    {
+        if (diff > 750 * 2 && diff < 2250 * 2 && chan < 8)    // 750 to 2250 ms is our 'valid' channel range
         {
             Inputs[chan].capture = diff;
         }
@@ -97,33 +95,36 @@ static void serialPWM_IRQHandler(TIM_TypeDef * tim)
 
 ///////////////////////////////////////////////////////////////////////////////
 
-static void parallelPWM_IRQHandler(TIM_TypeDef * tim)
+static void parallelPWM_IRQHandler(TIM_TypeDef *tim)
 {
     uint8_t i;
     uint16_t val = 0;
 
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < 8; i++)
+    {
         struct TIM_Channel channel = Channels[i];
         struct PWM_State *state = &Inputs[i];
 
-        if (channel.tim == tim && (TIM_GetITStatus(tim, channel.cc) == SET)) {
+        if (channel.tim == tim && (TIM_GetITStatus(tim, channel.cc) == SET))
+        {
             TIM_ClearITPendingBit(channel.tim, channel.cc);
-            if (i == 0)
+            if ( i == 0 )
                 rcActive = true;
 
-            switch (channel.channel) {
-            case TIM_Channel_1:
-                val = TIM_GetCapture1(channel.tim);
-                break;
-            case TIM_Channel_2:
-                val = TIM_GetCapture2(channel.tim);
-                break;
-            case TIM_Channel_3:
-                val = TIM_GetCapture3(channel.tim);
-                break;
-            case TIM_Channel_4:
-                val = TIM_GetCapture4(channel.tim);
-                break;
+            switch (channel.channel)
+            {
+                case TIM_Channel_1:
+                    val = TIM_GetCapture1(channel.tim);
+                    break;
+                case TIM_Channel_2:
+                    val = TIM_GetCapture2(channel.tim);
+                    break;
+                case TIM_Channel_3:
+                    val = TIM_GetCapture3(channel.tim);
+                    break;
+                case TIM_Channel_4:
+                    val = TIM_GetCapture4(channel.tim);
+                    break;
             }
 
             if (state->state == 0)
@@ -131,19 +132,19 @@ static void parallelPWM_IRQHandler(TIM_TypeDef * tim)
             else
                 state->fall = val;
 
-            if (state->state == 0) {
+            if (state->state == 0)
+            {
                 // switch states
                 state->state = 1;
 
                 TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Falling;
                 TIM_ICInitStructure.TIM_Channel = channel.channel;
                 TIM_ICInit(channel.tim, &TIM_ICInitStructure);
-            } else {
+            }
+            else
+            {
                 // compute capture
-                if (state->fall > state->rise)
-                    state->capture = (state->fall - state->rise);
-                else
-                    state->capture = ((0xffff - state->rise) + state->fall);
+                state->capture = state->fall - state->rise;
 
                 // switch state
                 state->state = 0;
@@ -188,90 +189,94 @@ void pwmInputInit(void)
     // RX7  TIM3_CH3 PB0
     // RX8  TIM3_CH4 PB1
 
-    GPIO_InitTypeDef GPIO_InitStructure = { 0, };
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure = { 0, };
-    NVIC_InitTypeDef NVIC_InitStructure = { 0, };
+    GPIO_InitTypeDef GPIO_InitStructure            = { 0, };
+    TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure = { 0, };
+    NVIC_InitTypeDef NVIC_InitStructure            = { 0, };
     uint8_t i;
 
-    if (systemConfig.useSerialPWMflag) {
+    if (systemConfig.useSerialPWMflag)
+    {
         // PPM input
         // Configure TIM2_CH1 for PPM input
-        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
+        GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_0;
+        GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IPD;
         GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
         GPIO_Init(GPIOA, &GPIO_InitStructure);
 
         // Input timer on TIM2 only for PPM
-        NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+        NVIC_InitStructure.NVIC_IRQChannel                   = TIM2_IRQn;
         NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-        NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-        NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+        NVIC_InitStructure.NVIC_IRQChannelSubPriority        = 1;
+        NVIC_InitStructure.NVIC_IRQChannelCmd                = ENABLE;
 
         NVIC_Init(&NVIC_InitStructure);
 
         // TIM2 timebase
         TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
 
-        TIM_TimeBaseStructure.TIM_Prescaler = (36 - 1);
-        TIM_TimeBaseStructure.TIM_Period = 0xffff;
+        TIM_TimeBaseStructure.TIM_Prescaler   = (36 - 1);
+        TIM_TimeBaseStructure.TIM_Period      = 0xffff;
         TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
 
         TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
 
         // Input capture on TIM2_CH1 for PPM
-        TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+        TIM_ICInitStructure.TIM_ICPolarity  = TIM_ICPolarity_Rising;
         TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
         TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
-        TIM_ICInitStructure.TIM_ICFilter = 0x0;
-        TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
+        TIM_ICInitStructure.TIM_ICFilter    = 0x0;
+        TIM_ICInitStructure.TIM_Channel     = TIM_Channel_1;
 
         TIM_ICInit(TIM2, &TIM_ICInitStructure);
 
         // TIM2_CH1 capture compare interrupt enable
         TIM_ITConfig(TIM2, TIM_IT_CC1, ENABLE);
         TIM_Cmd(TIM2, ENABLE);
-    } else {
-        // Configure TIM2, TIM3 all 4 channels
-        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_6 | GPIO_Pin_7;
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
+    }
+    else
+    {
+       // Configure TIM2, TIM3 all 4 channels
+        GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_6 | GPIO_Pin_7;
+        GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IPD;
         GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
         GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+        GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_0 | GPIO_Pin_1;
 
         GPIO_Init(GPIOB, &GPIO_InitStructure);
 
         // Input timers on TIM2 and TIM3 for PWM
-        NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+        NVIC_InitStructure.NVIC_IRQChannel                   = TIM2_IRQn;
         NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-        NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-        NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+        NVIC_InitStructure.NVIC_IRQChannelSubPriority        = 1;
+        NVIC_InitStructure.NVIC_IRQChannelCmd                = ENABLE;
 
         NVIC_Init(&NVIC_InitStructure);
 
-        NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+        NVIC_InitStructure.NVIC_IRQChannel                   = TIM3_IRQn;
 
         NVIC_Init(&NVIC_InitStructure);
 
         // TIM2 and TIM3 timebase
         TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
 
-        TIM_TimeBaseStructure.TIM_Prescaler = (36 - 1);
-        TIM_TimeBaseStructure.TIM_Period = 0xffff;
+        TIM_TimeBaseStructure.TIM_Prescaler   = (36 - 1);
+        TIM_TimeBaseStructure.TIM_Period      = 0xffff;
         TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
 
         TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
         TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
 
         // PWM Input capture
-        TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+        TIM_ICInitStructure.TIM_ICPolarity  = TIM_ICPolarity_Rising;
         TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
         TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
-        TIM_ICInitStructure.TIM_ICFilter = 0x0;
+        TIM_ICInitStructure.TIM_ICFilter    = 0x0;
 
-        for (i = 0; i < 8; i++) {
+        for (i = 0; i < 8; i++)
+        {
             TIM_ICInitStructure.TIM_Channel = Channels[i].channel;
             TIM_ICInit(Channels[i].tim, &TIM_ICInitStructure);
         }
